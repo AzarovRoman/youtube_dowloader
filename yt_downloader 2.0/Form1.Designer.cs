e80a74b3a54d24f47381077a350f3220b961e59f
AzarovRoman <azarovrom9215@gmail.com>
2021-05-11 21:52:48 +0300

    create design, add main fucntional

---
+++ yt_downloader 2.0/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿
+namespace yt_downloader_2._0
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 13);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(246, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(139, 55);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(120, 95);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Скачать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(13, 55);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 69);
+            this.listBox1.TabIndex = 9;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(13, 127);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(120, 23);
+            this.progressBar1.TabIndex = 10;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 153);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(129, 13);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Designed by AzarovCartel";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(268, 179);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        public System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ yt_downloader 2.0/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using VideoLibrary;
+using System.IO;
+
+namespace yt_downloader_2._0
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private string SelectFolder()
+        {
+            FolderBrowserDialog DirDialog = new FolderBrowserDialog();
+            DirDialog.Description = "Выбор директории";
+            //DirDialog.SelectedPath = @"C:\";
+
+            if (DirDialog.ShowDialog() == DialogResult.OK)
+            {
+                return DirDialog.SelectedPath;
+            }
+            return DirDialog.SelectedPath;
+        }
+
+        private void Download(string path, string url)
+        {
+            var youTube = YouTube.Default;
+
+            Console.WriteLine(listBox1.SelectedIndex.ToString());
+            Console.WriteLine(listBox1.Items.Count);
+            
+            var video = YouTube.Default.GetAllVideos(url).First(v => v.Resolution == int.Parse(listBox1.SelectedItem.ToString()));
+            var audio = YouTube.Default.GetVideo(url);
+
+            byte[] bytesvideo = video.GetBytes();
+            File.WriteAllBytes(path + video.FullName, bytesvideo);
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            List<int> allResolutuons = new List<int>();
+
+            var videoInfos = Client.For(YouTube.Default).GetAllVideosAsync(textBox1.Text).GetAwaiter().GetResult();
+            var resolutions = videoInfos.Where(j => j.AdaptiveKind == AdaptiveKind.Video).Select(j => j.Resolution);
+
+            foreach (int resol in resolutions)
+            {
+                if (allResolutuons.IndexOf(resol) == -1 && resol > 240 && resol < 1080)
+                {
+                    allResolutuons.Add(resol);
+                }
+            }
+
+            allResolutuons.Sort();
+            allResolutuons.Reverse();
+
+            foreach (int resol in allResolutuons)
+            {
+                Console.WriteLine(resol);
+                listBox1.Items.Add(resol);
+                Console.WriteLine(listBox1.Items.Count);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string path = SelectFolder() + "/";
+            string link = textBox1.Text;
+
+            Download(path, link);
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
